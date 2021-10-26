181834e71b79a579d95ec3f7c1f2d21e71e51a4f
Lee Min Gi <2019143077@yonsei.ac.kr>
2021-10-26 19:35:51 +0900

    Fix Host!!!!!!!!!!!!!!!!!!!!! #7

--- src/modules/host.js
+++ src/modules/host.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:8000";
 
-const BASE_URL = "http://3.38.102.201";
+const BASE_URL = "https://api2.planact.shop";
 // const BASE_URL = "";
 export default BASE_URL;
